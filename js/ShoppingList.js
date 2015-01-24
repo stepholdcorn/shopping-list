57e8e4bb0ce44359d86e3869276f33884d833f15
stepholdcorn <stephieoldcorn@gmail.com>
2015-01-24 19:49:50 +0000

    Adding new items

--- js/ShoppingList.js
+++ js/ShoppingList.js
@@ -14,7 +14,13 @@ $(document).ready(function() {
     $('li.hot').html(function() {
       return '<em>' + $(this).text() + '</em>';
     });
-    $('li#one').remove();
+  });
+
+  $(function() {
+    $('ul').before('<p class="notice">Just Updated</p>');
+    $('li.hot').prepend('+ ');
+    var $newListItem = $('<li><em>gluten-free</em> soy sauce</li>');
+    $('li:last').after($newListItem);
   });
 
 });
