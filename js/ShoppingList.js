f99af4d8dd3b3803fc7f02cf3efe6ba5394ebc94
stepholdcorn <stephieoldcorn@gmail.com>
2015-01-24 20:16:19 +0000

    Changing CSS rules

--- js/ShoppingList.js
+++ js/ShoppingList.js
@@ -1,29 +1,15 @@
 $(document).ready(function() {
 
-  $(':header').addClass('headline');
-  $('li:lt(3)').hide().fadeIn(1500);
-  $('li').on('click', function() {
-    $(this).remove();
-  });
-
-  $(function() {
-    $('li:contains("pine")').text('almonds');
-    $('li.hot').html(function() {
-      return '<em>' + $(this).text() + '</em>';
+$(function() {
+  var backgroundColor = $('li').css('background-color');
+    $('ul').append('<p>Color was: ' + backgroundColor + '</p>');
+    $('li').css({
+      'background-color': '#c5a996',
+      'border': '1px solid #fff',
+      'color': '#000',
+      'font-family': 'Georgia',
+      'padding-left': '+=75'
     });
-  });
-
-  $(function() {
-    $('ul').before('<p class="notice">Just Updated</p>');
-    $('li.hot').prepend('+ ');
-    var $newListItem = $('<li><em>gluten-free</em> soy sauce</li>');
-    $('li:last').after($newListItem);
-  });
-
-  $(function() {
-    $('li#three').removeClass('hot');
-    $('li.hot').addClass('favorite');
-    $('ul').attr('id', 'group');
   });
 
 });
