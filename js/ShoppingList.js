b64ff6674b5c2cfc9cbcf411c1df0048b5d890dc
stepholdcorn <stephieoldcorn@gmail.com>
2015-01-25 11:11:26 +0000

    Event object

--- js/ShoppingList.js
+++ js/ShoppingList.js
@@ -1,18 +1,11 @@
 $(document).ready(function() {
 
-  $(function() {
-    var ids = '';
-    var $listItems = $('li');
-
-    $listItems.on('mouseover click', function() {
-      ids = this.id;
-      $listItems.children('span').remove();
-      $(this).append('<span class="priority">' + ids + '</span>');
-    });
-
-    $listItems.on('mouseout', function() {
-      $(this).children('span').remove();
-    });
+  $('li').on('click', function(e) {
+    $('li span').remove();
+    var date = new Date();
+    date.setTime(e.timeStamp);
+    var clicked = date.toDateString();
+    $(this).append('<span class="date">' + clicked + ' ' + e.type + '</span>');
   });
 
 });
