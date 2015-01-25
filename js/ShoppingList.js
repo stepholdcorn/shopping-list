79eafaf9e2f19f3504dff431b5d365385cc18ffb
stepholdcorn <stephieoldcorn@gmail.com>
2015-01-25 12:04:35 +0000

    Using animation

--- js/ShoppingList.js
+++ js/ShoppingList.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
 
-  $('h2').hide().slideDown();
-  var $li = $('li');
-  $li.hide().each(function(index) {
-    $(this).delay(700 * index).fadeIn(700);
+  $('li').on('click', function() {
+    $(this).animate({
+      opacity: 0.0,
+      paddingLeft: '+=80'
+    }, 500, function() {
+      $(this).remove();
+    });
   });
-  $li.on('click', function() {
-    $(this).fadeOut(700);
-  });
+
 });
 
 
