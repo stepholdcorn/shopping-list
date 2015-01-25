378edbdcdc12da44cafd9730678d9ec2172b7d53
stepholdcorn <stephieoldcorn@gmail.com>
2015-01-25 12:26:35 +0000

    Uses filters

--- js/ShoppingList.js
+++ js/ShoppingList.js
@@ -1,16 +1,18 @@
 $(document).ready(function() {
 
-  var $h2 = $('h2');
-  $('ul').hide();
-  $h2.append('<a class="show">show</a>');
+var $listItems = $('li');
+$listItems.filter('.hot:last').removeClass('hot');
+$('li:not(.hot)').addClass('cool');
+$listItems.has('em').addClass('complete');
 
-  $h2.on('click', function() {
-    $h2.next()
-      .fadeIn(500)
-      .children('.hot')
-      .addClass('complete');
-    $h2.find('a').fadeOut();
-  });
+$listItems.each(function() {
+  var $this = $(this);
+  if ($this.is('.hot')) {
+    $this.prepend('Priority item: ');
+  }
+});
+
+$('li:contains("honey")').append(' (local)');
 
 });
 
