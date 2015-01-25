59e6b164f92ee06b732256293852e49261370167
stepholdcorn <stephieoldcorn@gmail.com>
2015-01-25 14:58:38 +0000

    Adds a form for new items

--- js/ShoppingList.js
+++ js/ShoppingList.js
@@ -1,5 +1,23 @@
 $(function() {
-  $('li:lt(2)').removeClass('hot');
-  $('li').eq(0).addClass('complete');
-  $('li:gt(2)').addClass('cool');
+
+  var $newItemButton = $('#newItemButton');
+  var $newItemForm = $('#newItemForm');
+  var $textInput = $('input:text');
+
+  $newItemButton.show();
+  $newItemForm.hide();
+
+  $('#showForm').on('click', function() {
+    $newItemButton.hide();
+    $newItemForm.show();
+  });
+
+  $newItemForm.on('submit', function(e) {
+    e.preventDefault();
+    var newText = $textInput.val();
+    $('li:last').after('<li>' + newText + '</li>');
+    $newItemForm.hide();
+    $newItemButton.show();
+    $textInput.val('');
+  });
 });
