f4ec2d575ff89bf7bdd43f149a67bcacd0542f29
stepholdcorn <stephieoldcorn@gmail.com>
2015-01-25 16:19:53 +0000

    Full example

--- js/ShoppingList.js
+++ js/ShoppingList.js
@@ -1,23 +1,61 @@
 $(function() {
 
-  var $newItemButton = $('#newItemButton');
-  var $newItemForm = $('#newItemForm');
-  var $textInput = $('input:text');
+  // SETUP
+  var $list, $newItemForm, $newItemButton;
+  var item = '';
+  $list = $('ul');
+  $newItemForm = $('#newItemForm');
+  $newItemButton = $('#newItemButton');
 
+  $('li').hide().each(function(index) {
+    $(this).delay(450 * index).fadeIn(1600);
+  });
+
+  // ITEM COUNTER
+  function updateCount() {
+    var items = $('li[class!=complete]').length;
+    $('#counter').text(items);
+  }
+  updateCount();
+
+  // SETUP FORM FOR NEW ITEMS
   $newItemButton.show();
   $newItemForm.hide();
-
   $('#showForm').on('click', function() {
     $newItemButton.hide();
     $newItemForm.show();
   });
 
+  // ADDING A NEW LIST ITEM
   $newItemForm.on('submit', function(e) {
     e.preventDefault();
-    var newText = $textInput.val();
-    $('li:last').after('<li>' + newText + '</li>');
-    $newItemForm.hide();
-    $newItemButton.show();
-    $textInput.val('');
+    var text = $('input:text').val();
+    $list.append('<li>' + text + '</li>');
+    $('input:text').val('');
+    updateCount();
   });
+
+  // CLICK HANDLING - USES DELEGATION ON <ul> ELEMENT
+  $list.on('click', 'li', function() {
+    var $this = $(this);
+    var complete = $this.hasClass('complete');
+
+    if (complete === true) {
+      $this.animate({
+        opacity: 0.0,
+        paddingLeft: '+=180'
+      }, 500, 'swing', function() {
+        $this.remove();
+      });
+    }
+    else {
+      item = $this.text();
+      $this.remove();
+      $list
+        .append('<li class=\"complete\">' + item + '</li>')
+        .hide().fadeIn(300);
+      updateCount();
+    }
+  });
+
 });
